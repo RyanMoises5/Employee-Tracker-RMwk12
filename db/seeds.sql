8ed1d649e172b2683e10539b4a5067500bbff5c1
Ryan M <ryan.a.moises@gmail.com>
2024-04-04 00:38:39 -0400

    Mysql View Employees displaying properly

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,8 @@ VALUES ("Sales"),
     ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Salesperson", 80000, 1),
+VALUES ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
     ("Lead Engineer", 150000, 2),
     ("Software Engineer", 120000, 3),
     ("Account Manager", 160000, 3),
@@ -14,11 +15,11 @@ VALUES ("Salesperson", 80000, 1),
     ("Lawyer", 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ryan", "Moises", 1, NULL),
-    ("Mike", "Chan", 1, 1),
-    ("Ashley", "Rodriguez", 2, NULL),
-    ("Kevin", "Tupik", 2, 3),
-    ("Kunal", "Singh", 3, NULL),
-    ("Malia", "Brown", 3, 5),
-    ("Sarah", "Lourd", 4, NULL),
-    ("Tom", "Allen", 4, 7);
+VALUES ("John", "Doe", 1, null),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 3, null),
+    ("Kevin", "Tupik", 4, 3),
+    ("Kunal", "Singh", 5, null),
+    ("Malia", "Brown", 6, 5),
+    ("Sarah", "Lourd", 7, null),
+    ("Tom", "Allen", 8, 7);
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require('express');
-const app = express();
+const inquirer = require('inquirer');
 
 const mysql = require('mysql2');
-const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection(
   {
@@ -14,13 +12,9 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-const initalPrompt = {
+const initialPrompt = {
   type: 'list',
-  name: 'userCommand',
+  name: 'command',
   message: 'What would you like to do?',
   choices: [
     'View All Employees',
@@ -34,19 +28,44 @@ const initalPrompt = {
     ]
 };
 
-const addEmployee = [{
+// View All Employees
+const viewEmployees = () => {
+    const sql = 
+      `SELECT 
+      employee.id, 
+      employee.first_name, 
+      employee.last_name,
+      role.title, 
+      department.name AS department,
+      role.salary,
+      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+      FROM employee
+      JOIN role ON employee.role_id = role.id
+      JOIN department ON role.department_id = department.id
+      LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        console.error("Error with request.");
+      }
+      console.table(rows);
+      promptUser();
+    });
+};
+
+// Add Employee (INCOMPLETE)
+const inquireAddEmployee = [{
     type: 'input',
-    name: 'first_name',
+    name: 'firstName',
     message: "What is the employee's FIRST name?"
   },
   {
     type: 'input',
-    name: 'last_name',
+    name: 'lastName',
     message: "What is the employee's LAST name?"
   },
   {
     type: 'list',
-    name: 'role_id',
+    name: 'roleId',
     message: "What is the employee's role?",
     choices: [
       'Salesperson',
@@ -60,20 +79,105 @@ const addEmployee = [{
   }
 ];
 
+const addEmployee = () => {
+  inquirer
+    .prompt(inquireAddEmployee)
+    .then((res) => {
+      const params = [res.firstName, res.lastName, res.roleID, res.managerID]
+      const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)'
+
+      db.query(sql, params, (err, rows) => {
+        if (err) {
+          console.error("Error with request.");
+        }
+        console.log("Added employee to database");
+        viewEmployees();
+      })
+    });
+};
+
+// Update Employee Role (INCOMPLETE)
 const updateEmployeeRole = {
   type: 'input',
   name: 'id',
   message: "Which employee role would you like to update?"
 };
 
+// View all roles (INCOMPLETE)
+const viewRoles = () => {
+  const sql = 
+    `SELECT 
+    employee.id, 
+    employee.first_name, 
+    employee.last_name,
+    role.title, 
+    department.name AS department,
+    role.salary,
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    FROM employee
+    JOIN role ON employee.role_id = role.id
+    JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.error("Error with request.");
+    }
+    console.table(rows);
+    promptUser();
+  });
+};
+
+// Add role (INCOMPLETE)
 const addEmployeeRole = {
   type: 'input',
   name: 'role',
   message: "What employee role would you like to add?"
 };
 
+// View All Departments (INCOMPLETE)
+
+// Add Department (INCOMPLETE)
 const addDepartment = {
   type: 'input',
   name: 'department',
   message: 'What department would you like to add?'
 };
+
+// Quit
+const quitInquirer = () => {
+  process.exit();
+};
+
+const promptUser = () => {
+  inquirer
+    .prompt(initialPrompt)
+    .then((res) => {
+        if (res.command === 'View All Employees') {
+          viewEmployees();
+        };
+        if (res.command === 'Add Employee') {
+          addEmployee();
+        };
+        if (res.command === 'Update Employee Role') {
+          console.log('PUT ROLE');
+        };
+        if (res.command === 'View All Roles') {
+          viewRoles();
+        };
+        if (res.command === 'Add Role') {
+          console.log('POST ROLE');
+        };
+        if (res.command === 'View All Departments') {
+          console.log('GET DEPARTMENTS');
+        };
+        if (res.command === 'Add Department') {
+          console.log('POST DEPARTMENT');
+        };
+        if (res.command === 'Quit') {
+          quitInquirer();
+        };
+    })
+};
+
+promptUser();
